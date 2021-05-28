68c57abd84cc79d5171c0b25f1a40034edbf5c3e
SrPollen <57717986+SrPollen@users.noreply.github.com>
2021-05-28 19:37:28 +0200

    restore fixed added teleport

--- Assets/Scripts/Game/TeleportPlayer.cs
+++ Assets/Scripts/Game/TeleportPlayer.cs
@@ -5,37 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class TeleportPlayer : MonoBehaviour
 {
-    // Type in the name of the Scene you would like to load in the Inspector
-    public string nextScene = "GameScene";
-
-    // Assign your GameObject you want to move Scene in the Inspector
-    public GameObject player;
-
+    public Vector3 tpPos = new Vector3(530, 8, 48);
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            StartCoroutine(LoadYourAsyncScene());
+            Debug.Log("hola");
+            other.gameObject.GetComponent<CharacterController>().Move(tpPos);
         }
-    }
-
-    IEnumerator LoadYourAsyncScene()
-    {
-        // Set the current Scene to be able to unload it later
-        Scene currentScene = SceneManager.GetActiveScene();
-
-        // The Application loads the Scene in the background at the same time as the current Scene.
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextScene, LoadSceneMode.Additive);
-
-        // Wait until the last operation fully loads to return anything
-        while (!asyncLoad.isDone)
-        {
-            yield return null;
-        }
-
-        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
-        SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByName(nextScene));
-        // Unload the previous Scene
-        SceneManager.UnloadSceneAsync(currentScene);
     }
 }
