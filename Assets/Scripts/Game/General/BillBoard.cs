fb3f8b078f0fb335cacde2fe859e648076683b40
SrPollen <57717986+SrPollen@users.noreply.github.com>
2021-06-09 19:43:53 +0200

    wave spawn working and increasing enemies

--- Assets/Scripts/Game/General/BillBoard.cs
+++ Assets/Scripts/Game/General/BillBoard.cs
@@ -1,15 +1,21 @@
 
+using System;
 using UnityEngine;
 
 public class BillBoard : MonoBehaviour
 {
     //para que las barras de vida miren siempre a la camara
     
-    public Transform cam;
-    
+    private Transform _cam;
+
+    private void Awake()
+    {
+        _cam = GameObject.Find("Main Camera").transform;
+    }
+
     void LateUpdate()
     {
         //coge la posicion actual y muentra en la direccion de la camara
-        transform.LookAt(transform.position +  cam.forward);
+        transform.LookAt(transform.position +  _cam.forward);
     }
 }
