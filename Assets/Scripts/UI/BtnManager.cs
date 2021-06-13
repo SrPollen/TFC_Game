d9ccc9932f3d4faaee32dd5f2f6157c2116624f5
SrPollen <57717986+SrPollen@users.noreply.github.com>
2021-06-13 05:09:13 +0200

    tutorial map

--- Assets/Scripts/UI/BtnManager.cs
+++ Assets/Scripts/UI/BtnManager.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BtnManager : MonoBehaviour
 {
     public AudioClip overSound, clickSound;
     private AudioSource _audioSource;
+    private string gameScene = "PreGameScene";
+    private string creditsScene = "Credits";
+    private string loginScene = "LoginScene";
+    
 
     private void Start()
     {
@@ -19,4 +24,27 @@ public class BtnManager : MonoBehaviour
         _audioSource.PlayOneShot(overSound, 1);
     }
 
+    public void ExitAction()
+    {
+        Application.Quit();
+    }
+
+    public void PlayAction()
+    {
+        Debug.Log("Play");
+        SceneManager.LoadScene(gameScene);
+    }
+
+    public void CreditAction()
+    {
+        Debug.Log("Credits");
+        SceneManager.LoadScene(creditsScene);
+    }
+
+    public void LogOutAction()
+    {
+        Debug.Log("LogOut");
+        SceneManager.LoadScene(loginScene);
+    }
 }
+
